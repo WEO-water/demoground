e6b6738fc8b225022d3d5c90e382d6e5cd725fcf
Thierry Nicola <tnicola@weo-water.com>
2023-03-17 17:04:30 +0100

    Adding GH Pages deployment

---
+++ gh-pages.js
@@ -0,0 +1,18 @@
+import ghpages from 'gh-pages';
+
+
+ghpages.publish(
+    'build', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/WEO-water/demoground.git', // Update to point to your repository  
+        user: {
+            name: 'thierryweo', // update to use your name
+            email: 'tnicola@weo-water.com' // Update to use your email
+        },
+        dotfiles: true,
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
