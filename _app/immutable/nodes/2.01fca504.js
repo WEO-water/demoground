5af20393f6fc9914e011de441956e97545e8f19a
thierryweo <tnicola@weo-water.com>
2024-01-12 12:13:19 +0100

    Updates

--- _app/immutable/nodes/2.01fca504.js
+++
@@ -1,1 +0,0 @@
-import{P as e,_ as n}from"../chunks/2.8ab9848f.js";export{e as component,n as universal};
---
+++ _app/immutable/nodes/2.c0064d68.js
@@ -0,0 +1,1 @@
+import{P as e,_ as n}from"../chunks/2.c293a5f6.js";export{e as component,n as universal};
